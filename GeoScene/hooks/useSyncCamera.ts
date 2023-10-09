0bc73270f61dca949e86715d995d0686e2a2cd7b
wenhu yang <36725799+Preyou@users.noreply.github.com>
2023-10-09 09:31:58 +0800

    Delete GeoScene directory

--- GeoScene/hooks/useSyncCamera.ts
+++
@@ -1,7 +0,0 @@
-import { computedWithControl } from '@vueuse/core'
-
-export function useSyncCamera(view: Vue.WatchSource<GeoScene.SceneView>) {
-  return computedWithControl(view, () => {
-    return {}
-  })
-}
--- GeoScene/symbols/index.ts
+++
@@ -1,3 +0,0 @@
-export const viewSymbol = Symbol('view')
-export const sceneViewSymbol = Symbol('sceneView')
-export const mapViewSymbol = Symbol('mapView')
